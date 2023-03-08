427630444ee51248c98ba4cbc8e6dbfdf6812475
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2023-03-08 13:01:13 +0300

    продолжение

---
+++ sicharp/Program.cs
@@ -0,0 +1,15 @@
+﻿// Ввод чисел
+
+Console.Clear(); // очистка консоли
+// int n = Convert.ToInt32(Console.ReadLine());
+int n=int.Parse(Console.ReadLine()!);
+Console.WriteLine(n*3);
+// ctrl + / - комментирование и раскомментирование
+/*
+программный код
+Hello. world!
+*/
+
+// Необходимо спросить у пользователя 2 числа и узнать , какое из них наибольшее.
+// 1 > 2
+// 2 > 1
