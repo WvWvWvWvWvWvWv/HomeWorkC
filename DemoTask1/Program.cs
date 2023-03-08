6f59bbd6573484b502284535adf7cdf9c8493166
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2023-03-08 15:20:34 +0300

    Add DemoTask3

--- DemoTask1/Program.cs
+++ DemoTask1/Program.cs
@@ -1,4 +1,12 @@
 ﻿Console.Clear();
 Console.Write("Введите число : ");
-int n = int.Parse(Console.ReadLine()!);
-Console.WriteLine(n * n);
+int n =int.Parse(Console.ReadLine()!);
+Console.Write("Введите число : ");
+int b =int.Parse(Console.ReadLine()!);
+if (n/b==b)
+    Console.WriteLine("da");
+else
+    Console.WriteLine("net");
+
+
+
