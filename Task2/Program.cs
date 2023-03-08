79b54dc6f18cb4713a89ccc21faa3e1d5deedc88
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2023-03-08 16:38:48 +0300

    Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+if (n>a)
+    {
+    Console.WriteLine(n + ">" + a);
+    }
+else 
+    Console.WriteLine(a + ">" + n);
