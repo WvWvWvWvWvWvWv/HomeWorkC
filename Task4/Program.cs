f97784293467aa4b7c26538977c5928d4bc040b8
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2023-03-08 17:22:29 +0300

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int z = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int c = Convert.ToInt32(Console.ReadLine());
+if (z>x)
+{
+    if(z>c)
+    Console.WriteLine("max " + z);
+}
+if( x>z)
+{   
+    if(x>c)
+    Console.WriteLine("max " + x);
+}
+if (c>x)
+{
+    if(c>z)
+    Console.WriteLine("max " + c);
+
+}
+
+        
+
+
