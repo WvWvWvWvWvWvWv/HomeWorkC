4eeceb5cde2b9ade69367268208aad830572ba98
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2023-03-08 15:45:49 +0300

    Add Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), i = (-1) * n;
+n = Math.Abs(n); // модуль числа
+// for (int i = (-1) * n; i <= n; i++)
+//     Console.Write($"{i} ");
+while (i <= n)
+{
+    Console.Write($"{i} ");
+    i++;
+}
