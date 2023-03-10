3c905cee55a59f71cf0dc32e2f8954c3070fc7e1
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2023-03-10 17:21:54 +0300

    add Task13/

---
+++ Task13/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите трехзначное число ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 100)
+    Console.WriteLine("3 цифры нет, введите трехзначное число!");
+while (a>1000)
+     a=a/10;
+    Console.WriteLine(a%10);
