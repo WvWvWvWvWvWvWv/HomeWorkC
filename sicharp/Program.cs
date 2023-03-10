317bc3a58626eb14bbb54aefd33ac4539318c10b
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2023-03-10 14:06:48 +0300

    Семинар 2
    Семинар 2

--- sicharp/Program.cs
+++ sicharp/Program.cs
@@ -32,3 +32,48 @@ Hello. world!
 // for(int i=10; i<=10; i--)
 //     Console.Write($"{i} "); // 10 9 8 7 6 5 4 3 2 1 
 // Console.WriteLine();
+// Console.Clear();
+// int n= new Random().Next(10,100); //[10,99]
+// Console.WriteLine($"случайное число {n}");
+// int n1 =n/10;
+// int n2 =n%10;
+// if (n1>n2)
+//     Console.WriteLine(n1);
+// else    
+//     Console.WriteLine(n2);
+// Console.Clear();
+// int n= new Random().Next(100,1000); //[100,999]
+// Console.WriteLine($"случайное число {n}");
+// int n1=n/100;
+// int n3=n%10;
+// Console.WriteLine($"{n1}{n3}");// Console.WriteLine(n1*10+n3);
+// Console.Clear();
+// Console.WriteLine("Введите первое число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if(a%b==0)
+//     Console.WriteLine("Кратно");
+// else
+//     Console.WriteLine($"Не кратно,остаток {a%b} ");
+// Console.Clear();
+// Console.WriteLine("Введите число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// if (a%7==0)
+//     Console.WriteLine("yes");
+// else 
+//     Console.WriteLine("No");
+Console.Clear();
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a*a==b || b*b==a)
+    Console.WriteLine("Да");
+else
+    Console.WriteLine("Нет");
+
+// || или 
+// && и
+
+
