60b4c9bfba9b2e4820fc424ca0c532dddb886358
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2023-03-11 19:52:05 +0300

    Исправление task 13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -3,6 +3,7 @@ Console.Write("Введите трехзначное число ");
 int a = Convert.ToInt32(Console.ReadLine());
 if (a < 100)
     Console.WriteLine("3 цифры нет, введите трехзначное число!");
+else {
 while (a>1000)
      a=a/10;
-    Console.WriteLine(a%10);
+    Console.WriteLine(a%10);}
