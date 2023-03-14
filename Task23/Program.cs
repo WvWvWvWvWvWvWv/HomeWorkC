911851eec61a186c0f2715b68daba4ddc3c0fb2c
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2023-03-14 14:48:55 +0300

    Add Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+System.Console.WriteLine("Введите число : ");
+int n  = Convert.ToInt32(Console.ReadLine());
+for (int i = 1;  i <= n ; i++ )
+    System.Console.WriteLine($"{i} ^ 3 = {i * i * i}");
