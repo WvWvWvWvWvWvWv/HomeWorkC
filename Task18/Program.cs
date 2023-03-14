8ec487916e3d6dcc06fac7147ce6ce1ab797cdda
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2023-03-14 14:25:26 +0300

    Lesson3

---
+++ Task18/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите номер четверти: ");
+int n = int.Parse(Console.ReadLine()!);
+while (n < 1 || n > 4)
+{
+    Console.Write("Вы ошиблись!\nВведите номер четверти: ");
+    n = int.Parse(Console.ReadLine()!);
+}
+if (n == 1)
+    Console.WriteLine("x > 0 and y > 0");
+else if (n == 2)
+    Console.WriteLine("x < 0 and y > 0");
+else if (n == 3)
+    Console.WriteLine("x < 0 and y < 0");
+else
+    Console.WriteLine("x > 0 and y < 0");
---
+++ test/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+string s = Console.ReadLine()!;
+int sum = 0;
+// Напишите программу, которая будет находить сумму цифр числа
+for (int i = 0; i < s.Length; i++)
+    sum = sum + int.Parse(s[i].ToString());
+Console.WriteLine(sum);
