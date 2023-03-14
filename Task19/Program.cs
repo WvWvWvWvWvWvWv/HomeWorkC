53a1c8c76d29d7bf0b95fc166e7d7e67658b4f06
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2023-03-14 14:31:48 +0300

    Add Task19/

---
+++ Task19/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+System.Console.WriteLine("Введите пятизначное число : ");
+int a = Convert.ToInt32(Console.ReadLine());
+while (a<10000 || a>99999){
+    Console.Write("Вы ошиблись!\nВведите пятизначное число : ");
+    a = Convert.ToInt32(Console.ReadLine());
+}
+if (a/10000%10==a%10 || a/1000%10==a/10%10){
+    System.Console.WriteLine("True");
+}
+else
+    System.Console.WriteLine("False");
