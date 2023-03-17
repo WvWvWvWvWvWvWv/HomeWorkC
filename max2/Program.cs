5464bdee8d91f373b8a328b233c25f5355b21049
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2023-03-17 13:12:51 +0300

    add max2/

---
+++ max2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+int n =  int.Parse(Console.ReadLine()!), max1=n, max2=0;
+while ( n!=0)
+{
+    n = int.Parse(Console.ReadLine()!);
+    if(n>max1)
+    {
+        max2=max1;
+        max1=n;
+
+    }
+    else if (n>max2)
+    {
+        max2=n;
+    }
+}
+System.Console.WriteLine("max = " + max2);
