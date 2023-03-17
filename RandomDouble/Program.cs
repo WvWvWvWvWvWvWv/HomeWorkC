a4fbca9818a9ec9531a4035f05b7f884f458c7dd
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2023-03-17 14:15:39 +0300

    Add RandomDouble/ Task26/ Task28/

---
+++ RandomDouble/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+System.Console.WriteLine("Vvedite chislo: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+// ... = new Random.NextDouble() * 9end - begin) +begin
+for ( int i = 0 ; i < array.Length; i++){
+    array[i] = Math.Round(new Random().NextDouble() * (30-10) + 10, 2);
+}
+System.Console.WriteLine($"[{string.Join(", " , array)}]");
---
+++ Task26/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+System.Console.WriteLine("Vvedite chislo: ");
+int n = Convert.ToInt32(Console.ReadLine()!);
+int a = 1;
+while ( n>9){
+    n/=10;
+    a++;
+
+}
+System.Console.WriteLine(a);
