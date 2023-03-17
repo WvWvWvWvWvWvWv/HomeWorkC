a95b8cc831902acc0802ed1aad8206437511aa44
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2023-03-17 13:26:18 +0300

    add Task24/ and test1.Array/

---
+++ Task24/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Clear();
+System.Console.WriteLine("Введите число: ");
+double n = Convert.ToInt32(Console.ReadLine()!);
+System.Console.WriteLine($"Результат {(1 + n) / 2 * n}");
---
+++ test1.Array/Program.cs
@@ -0,0 +1,9 @@
+﻿// Array
+Console.Clear();
+System.Console.WriteLine("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i <array.Length; i++){
+    array[i] = new Random().Next(1,11); // [1,10]
+    System.Console.WriteLine($"[{string.Join(", " , array)}]");
+}
