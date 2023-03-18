53fd3b8c01cf5aca13a62a7bd054e3fb43ddce7e
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2023-03-18 12:51:09 +0300

    Add Task27/

---
+++ Task27/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+System.Console.WriteLine("Введите число: ");
+int i = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while(i>0){
+    int a = i%10;
+    sum+=a;
+    i/=10;
+}
+System.Console.WriteLine(sum);
