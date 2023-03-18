8df5a18648dd91f47565f47abbcc1d22053a970c
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2023-03-18 20:54:56 +0300

    Исправил goldbah

--- GoldBah/Program.cs
+++ GoldBah/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.Clear();
 System.Console.WriteLine("Введите число: ");
 int n=Convert.ToInt32(Console.ReadLine());
-int random=new Random().Next(1,n);
+int random=new Random().Next(); // int random=new Random().Next(0,n) , но по сути правильнее так конечно.
 System.Console.WriteLine($"{n-random} + {random} = {n}");
