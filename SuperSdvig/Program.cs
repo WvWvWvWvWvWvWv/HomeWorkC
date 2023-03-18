0efbba83e2b209cec3893e74ecbb30a21379c971
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2023-03-18 15:34:52 +0300

    SuperSdvig

--- SuperSdvig/Program.cs
+++ SuperSdvig/Program.cs
@@ -6,39 +6,26 @@ int i = 0;
 int j = 0;
 int box = 0;
 int l = array.Length;
-// if(K>0){
-//     while (K>j){
-//     i=0;
-//     while(i<1){
-//         box=array[l-1];
-//         array[i+4]=array[i+3];
-//         array[i+3]=array[i+2];
-//         array[i+2]=array[i+1];
-//         array[i+1]=array[i];
-//         array[i]=box;
-//         i++;}
-//      j++;}}
-// if(K<0){
-//     while(K<j){
-//         i=0;
-//         while(i<1){
-//         box=array[0];
-//         array[i]=array[i+1];
-//         array[i+1]=array[i+2];
-//         array[i+2]=array[i+3];
-//         array[i+3]=array[i+4];
-//         array[l-1]=box;
-//         i++;}
-//         j--;}}
 while(K>j){
     i=0;
     box=array[l-1];
     while(i<l-1){
+        
         array[l-1-i]=array[l-2-i];
         i++;
     }
     array[0]=box;
     j++;
+}
+while(K<j){
+    i=0;
+    box=array[0];
+    while(i<l-1){
+        array[i]=array[i+1];
+        i++;
+    }
+    array[l-1]=box;
+    j--;
 }
  
 i=0;
