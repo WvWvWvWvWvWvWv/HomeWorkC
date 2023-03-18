c604b8d5dee392d1ace1a59e75050b9f54e8a5d9
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2023-03-18 12:44:29 +0300

    add Task25/

---
+++ Task25/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+System.Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("В какую степень желаете возвести? : ");
+int y = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int z= x;
+while ( i < y){
+    z=z*x;
+    i++;
+}
+
+System.Console.WriteLine(z);
