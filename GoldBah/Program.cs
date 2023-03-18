51fbb0b5d21eb4f9b03e26e78b6887de5a2ab04b
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2023-03-18 20:50:53 +0300

    так вернее goldbah

--- GoldBah/Program.cs
+++ GoldBah/Program.cs
@@ -2,4 +2,5 @@
 System.Console.WriteLine("Введите число: ");
 int n=Convert.ToInt32(Console.ReadLine());
 int random=new Random().Next(1,n);
-System.Console.WriteLine($"{n} - {random} = {n-random}");
+int y = n-random;
+System.Console.WriteLine($"{y} + {random} = {n}");
