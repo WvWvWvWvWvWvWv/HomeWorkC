52cb1d8b9ad196f75dbfe503999078d97df53761
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2023-03-18 13:10:21 +0300

    add Task29/

---
+++ Task29/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+int [] array = new int[8];
+Random random = new Random();
+int i = 0;
+while (i < array.Length){
+    array[i]=random.Next(0,10);
+    Console.Write($"{array[i]}, ");
+    i++;
+}
