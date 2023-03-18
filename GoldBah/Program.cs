97c7beeace6b6de77ec900c1afc8ee3ed7e11bb0
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2023-03-18 20:38:47 +0300

    GoldBah/

---
+++ GoldBah/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+System.Console.WriteLine("Введите число: ");
+int n=Convert.ToInt32(Console.ReadLine());
+int random=new Random().Next(1,n);
+System.Console.WriteLine($"{n} - {random} = {n-random}");
