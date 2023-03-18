c36ab41b8f36f49b34b09a918e1819d3383c9cc7
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2023-03-18 15:20:41 +0300

    SuperSdvig для K>0

--- SuperSdvig/Program.cs
+++ SuperSdvig/Program.cs
@@ -4,31 +4,45 @@ System.Console.WriteLine("Vvedite chislo: ");
 int K = Convert.ToInt32(Console.ReadLine());
 int i = 0;
 int j = 0;
-int box;
+int box = 0;
 int l = array.Length;
-if(K>0){
-    while (K>j){
+// if(K>0){
+//     while (K>j){
+//     i=0;
+//     while(i<1){
+//         box=array[l-1];
+//         array[i+4]=array[i+3];
+//         array[i+3]=array[i+2];
+//         array[i+2]=array[i+1];
+//         array[i+1]=array[i];
+//         array[i]=box;
+//         i++;}
+//      j++;}}
+// if(K<0){
+//     while(K<j){
+//         i=0;
+//         while(i<1){
+//         box=array[0];
+//         array[i]=array[i+1];
+//         array[i+1]=array[i+2];
+//         array[i+2]=array[i+3];
+//         array[i+3]=array[i+4];
+//         array[l-1]=box;
+//         i++;}
+//         j--;}}
+while(K>j){
     i=0;
-    while(i<1){
-        box=array[l-1];
-        array[i+4]=array[i+3];
-        array[i+3]=array[i+2];
-        array[i+2]=array[i+1];
-        array[i+1]=array[i];
-        array[i]=box;
-        i++;}
-     j++;}}
-if(K<0){
-    while(K<j){
-        i=0;
-        while(i<1){
-        box=array[0];
-        array[i]=array[i+1];
-        array[i+1]=array[i+2];
-        array[i+2]=array[i+3];
-        array[i+3]=array[i+4];
-        array[l-1]=box;
-        i++;}
-        j--;}}
+    box=array[l-1];
+    while(i<l-1){
+        array[l-1-i]=array[l-2-i];
+        i++;
+    }
+    array[0]=box;
+    j++;
+}
  
-Console.Write($"{array[0]},{array[1]},{array[2]},{array[3]},{array[4]}");
+i=0;
+while(i<array.Length){
+    Console.Write(array[i] + ", ");
+    i++;
+}
