8e05f1c598ee0a021d626c2035640e365f525647
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2023-03-21 18:59:04 +0300

    Add Task34/

---
+++ Task34/Program.cs
@@ -0,0 +1,23 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100 , 1000);
+}
+int Chetnost(int[] array)
+{
+    int count=0;
+    for(int i = 0;i<array.Length;i++)
+    {   if(array[i]%2==0){
+        count+=1;}
+    }
+    return count;
+}
+
+
+
+
+Console.Clear();
+int[] array = new int[10];
+InputArray(array);
+System.Console.WriteLine($"array: [{string.Join(" , " , array)}]");
+System.Console.WriteLine(Chetnost(array));
