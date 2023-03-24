d38c48ac61c7afc59fd8519c13e9141de7a2e0ee
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2023-03-24 13:31:23 +0300

    GoldBah

--- GoldBah/Program.cs
+++ GoldBah/Program.cs
@@ -1,5 +1,27 @@
 ﻿Console.Clear();
-System.Console.WriteLine("Введите число: ");
-int n=Convert.ToInt32(Console.ReadLine());
-int random=new Random().Next(0,n);
-System.Console.WriteLine($"{n-random} + {random} = {n}");
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int countDel = 0, i, j, k, m;
+for (i = 2; i <= Math.Sqrt(n) + 1; i++)
+{
+    countDel = 0;
+    for (j = 2; j <= Math.Sqrt(i) + 1; j++)
+    {
+        if (i % j == 0)
+            countDel++;
+    }
+    if (countDel == 0)
+    {
+        m = n - i;
+        for (k = 2; k <= Math.Sqrt(m) + 1; k++)
+        {
+            if (m % k == 0)
+                countDel++;
+        }
+        if (countDel == 0)
+        {
+            Console.WriteLine($"{i} {m}");
+            return;
+        }
+    }
+}
