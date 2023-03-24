bd6d6dedc3cc56a7666c185069c16f0a7820bbf2
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2023-03-24 19:03:10 +0300

    add 45,43,ploshad'treugolnika

---
+++ Task41/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+void AddArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {   
+        System.Console.WriteLine("Введите число: ");
+        array[i]=int.Parse(Console.ReadLine()!);
+    }
+}
+
+Console.Clear();
+System.Console.WriteLine("Сколько чисел планируете ввести?");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int [n];
+AddArray(array);
+int countplus=0;
+int countminus=0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i]>0)
+        {
+            countplus+=1;
+        }
+        // else
+        //     countminus+=1;
+    }
+System.Console.WriteLine($"Чисел больше 0: {countplus}");
+// System.Console.WriteLine($"Отрицательных чисел: {countminus}");
+
+
---
+++ Task43/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+System.Console.WriteLine("Введите b1: ");
+double b1 = double.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Введите k1: ");
+double k1 = double.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Введите b2: ");
+double b2 = double.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Введите k2: ");
+double k2 = double.Parse(Console.ReadLine()!);
+double x =(b2-b1)/(k1-k2);
+double y1 = k1*x+b1;
+double y2 = k2*x+b2;
+System.Console.WriteLine($"({y1};{y2})");
