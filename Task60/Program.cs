fd9c5949ba9302a7445046339036b0b319c58e51
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2023-04-06 11:01:52 +0300

    add 58,60,61,62

---
+++ Task60/Program.cs
@@ -0,0 +1,34 @@
+﻿void InputMatrix(int[,,] matrix)
+{
+    int number = 10;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = number++;
+            }
+        }
+
+    }
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            for (int k = 0; k < matrix.GetLength(2); k++)
+                Console.Write($"{matrix[i, j, k]} ({i},{j},{k}) \t");
+        Console.WriteLine();
+    }
+}
+
+
+Console.Clear();
+System.Console.Write("Введите размерность 3мерного массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,,] matrix = new int[size[0], size[1], size[2]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
