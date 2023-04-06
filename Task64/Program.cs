6bf4675610917f0acd54b3447e3bb7428a23af65
WvWvWvWvWvWvWv <126243680+WvWvWvWvWvWvWv@users.noreply.github.com>
2023-04-06 11:16:31 +0300

    add 64,66

---
+++ Task64/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+string f(int n)
+{
+    if (n == 1)
+        return "1 ";
+    return $"{n} " + f(n - 1);
+}
+
+
+Console.Clear();
+System.Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine(f(n));
---
+++ Task66/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int f(int m, int n)
+{
+    if (m == n)
+        return m;
+    return f(m, n - 1) + n;
+}
+
+
+
+
+Console.Clear();
+System.Console.Write("Введите начальное число: ");
+int m = int.Parse(Console.ReadLine()!);
+System.Console.Write("Введите конечное число: ");
+int n = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine(f(m, n));
+
